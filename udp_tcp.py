6a5bbeccdf192638dd7ec81362f60d1856258e8e
cuixiyi <cuixiyi@meetstarinc.com>
2019-09-12 17:59:45 +0800

    tcp/udp

--- udp_tcp.py
+++ udp_tcp.py
@@ -5,6 +5,7 @@ import socket
 import argparse
 import requests
 import sys
+import select, utils
 
 def server():
     HOST = '127.0.0.1'
@@ -61,6 +62,34 @@ threading.Thread(target=_accept, args=(sock,)).start()
 
 while not event.wait(2):
     logging.info(sock)
+
+
+# 异步服务器
+
+
+def all_listen_event(poll):
+    while True:
+        for fd, event in poll.poll():
+            yield fd, event
+
+def server(listen):
+    sockets = {listen.fileno(): listen}
+    recieve = ''
+    send = ''
+    poll = select.poll()
+    poll.register(listen, select.POLLIN)
+    for fd, event in all_listen_event(poll):
+        sock = sockets[fd]
+        if event and (select.POLLHUP or select.POLLNVAL or select.POLLERR):
+            """
+                开始接受/发送数据处理。
+            """
+            poll.unregister(sock)
+            del sockets[fd]
+        elif sock is listen:
+            client_sock, address = sock.accept()
+
+
 
 
 if __name__ == '__main__':
