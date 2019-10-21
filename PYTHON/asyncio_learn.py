d1fc808bb2d5b75d68dca7695c81b289c1472db6
cuixiyi <cuixiyi@meetstarinc.com>
2019-10-21 18:20:43 +0800

    linux 命令学习

--- PYTHON/asyncio_learn.py
+++ PYTHON/asyncio_learn.py
@@ -6,121 +6,168 @@ import time
 
 
 async def say_after(delay, what):
-	await asyncio.sleep(delay)
-	print(what)
+    await asyncio.sleep(delay)
+    print(what)
 
 
 async def main():
-	print(f"started at {time.strftime('%X')}")
+    print(f"started at {time.strftime('%X')}")
 
-	await say_after(1, 'hello')
-	await say_after(2, 'world')
+    await say_after(1, 'hello')
+    await say_after(2, 'world')
 
-	print(f"finished at {time.strftime('%X')}")
+    print(f"finished at {time.strftime('%X')}")
 
 
 async def main_1():
-	task1 = asyncio.create_task(
-		say_after(1, 'hello'))
+    task1 = asyncio.create_task(
+        say_after(1, 'hello'))
 
-	task2 = asyncio.create_task(
-		say_after(2, 'world'))
+    task2 = asyncio.create_task(
+        say_after(2, 'world'))
 
-	print(f"started at {time.strftime('%X')}")
+    print(f"started at {time.strftime('%X')}")
 
-	# Wait until both tasks are completed (should take
-	# around 2 seconds.)
-	await task1
-	await task2
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
 
-	print(f"finished at {time.strftime('%X')}")
+    print(f"finished at {time.strftime('%X')}")
 
 
 async def clear():
-	yield 'a'
+    yield 'a'
 
 
 def generator3():
-	num = yield 1
+    num = yield 1
 
-	print(num)
-	try:
-		yield 2
-	except ValueError:
-		print("捕获异常：ValueError")
-	yield 3
+    print(num)
+    try:
+        yield 2
+    except ValueError:
+        print("捕获异常：ValueError")
+    yield 3
 
 
 def run_generator3():
-	gen = generator3()
-	print(gen.send(None))
-	print(gen.send(101))
-	print(gen.send(None))
-	gen.throw(ValueError)
-	gen.close()
+    gen = generator3()
+    print(gen.send(None))
+    print(gen.send(101))
+    print(gen.send(None))
+    gen.throw(ValueError)
+    gen.close()
 
 
 def generator01():
-	num = yield 1
-	print(num)
-	yield 2
-	yield 3
-	return 4
+    num = yield 1
+    print('gen', num)
+    yield 2
+    yield 3
+    return 4
 
 
 def test01(gen):
-	ll = yield from gen()
-	print(ll)
+    while True:
+        ll = yield from gen()
+        gen().close()
+        print('proxy', ll)
 
 
 def run_main():
-	gen = test01(generator01)
-	print(gen.send(None))
-	print(gen.send(101))
-	# print(gen.throw(ValueError))
-	print(gen.send(None))
-	print(gen.send(None))
+    gen = test01(generator01)
+    print(type(gen), gen.__str__())
+    print(gen.send(None))
+    print(gen.send(101))
+    # print(gen.throw(ValueError))
+    print(gen.send(None))
+    print(gen.send(None))
+    # gen.throw(StopIteration)
+    print(gen.send(None))
+    print(gen.send(None))
+
 
 
 import requests
 
 
 async def request(url):
-	return requests.get(url, proxies={'http': 'socks5://192.168.2.244:9091',
-									  'https': 'socks5://192.168.2.244:9091'})
+    return requests.get(url, proxies={'http': 'socks5://192.168.2.244:9091',
+                                      'https': 'socks5://192.168.2.244:9091'})
 
 
 async def spider(url):
-	return await request(url)
+    return await request(url)
 
 
 def run_spider():
-	sp = spider("http://www.baidu.com", )
-	print(asyncio.run(spider("http://www.baidu.com")))
-	try:
-		print(sp.send(None))
-	except StopIteration as e:
-		print(e.value)
+    sp = spider("http://www.baidu.com", )
+    print(asyncio.run(spider("http://www.baidu.com")))
+    try:
+        print(sp.send(None))
+    except StopIteration as e:
+        print(e.value)
 
 
 async def factorial(name, number):
-	f = 1
-	for i in range(2, number + 1):
-		print(f"Task {name}: Compute factorial({i})...")
-		await asyncio.sleep(1)
-		f *= i
-	print(f"Task {name}: factorial({number}) = {f}")
-	return name
+    f = 1
+    for i in range(2, number + 1):
+        print(f"Task {name}: Compute factorial({i})...")
+        await asyncio.sleep(1)
+        f *= i
+    print(f"Task {name}: factorial({number}) = {f}")
+    return name
+
 
 async def run_factorial():
-	# Schedule three calls *concurrently*:
-	return await asyncio.gather(
-		factorial("A", 2),
-		factorial("B", 3),
-		factorial("C", 4),
-	)
+    # Schedule three calls *concurrently*:
+    return await asyncio.gather(
+        factorial("A", 2),
+        factorial("B", 3),
+        factorial("C", 4),
+    )
+
+
+final_result = {}
+
+
+def sales_sum(pro_name):
+    total = 0
+    nums = []
+    while True:
+        x = yield
+        print(pro_name + '销量', x)
+        if not x:
+            break
+        total += x
+        nums.append(x)
+    return total, nums  # 程序运行到return的时候，会将return的返回值返回给委托方，即middle中的final_result[key]
+
+
+def middle(key):
+    while True:  # 相当于不停监听sales_sum是否有返回数据，（本例中有三次返回）
+        final_result[key] = yield from sales_sum(key)
+        print(key + '销量统计完成！！')
+
+
+def main_2():
+    data_sets = {
+        '面膜': [1200, 1500, 3000],
+        '手机': [88, 100, 98, 108],
+        '衣服': [280, 560, 778, 70],
+    }
+
+    for key, data_set in data_sets.items():
+        print('start key', key)
+        m = middle(key)
+        m.send(None)  # 预激生成器
+        for value in data_set:
+            m.send(value)
+        m.send(None)  # 发送一个None使sales_sum中的x值为None退出while循环
+
+    print(final_result)
 
 
 if __name__ == "__main__":
-	results = asyncio.run(run_factorial())
-	print(results)
+    run_main()
--- PYTHON/python_test.py
+++ PYTHON/python_test.py
@@ -7,115 +7,130 @@ import fcntl
 import select
 from multiprocessing import Process, Pool
 
+
 class A(object):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def kill_people(self):
-		print(self, type(self))
-		a = A()
-		print(a)
+    def kill_people(self):
+        print(self, type(self))
+        a = A()
+        print(a)
 
-	@classmethod
-	def life_people(cls):
-		print(cls, type(cls))
+    @classmethod
+    def life_people(cls):
+        print(cls, type(cls))
 
-	@staticmethod
-	def hard_people():
-		print('execute people')
+    @staticmethod
+    def hard_people():
+        print('execute people')
 
 
 class B(A):
-	pass
+    pass
 
 
 import functools
+
+
 def decorator1(funcs):
-	print('start decorator1')
-	@functools.wraps(funcs)
-	def wrap(*args, **kwargs):
-		print('exec decorator1')
-		return funcs(*args, **kwargs)
-	return wrap
+    print('start decorator1')
+
+    @functools.wraps(funcs)
+    def wrap(*args, **kwargs):
+        print('exec decorator1')
+        return funcs(*args, **kwargs)
+
+    return wrap
+
 
 def decorator2(funcs):
-	print('start decorator2')
-	@functools.wraps(funcs)
-	def wrap(*args, **kwargs):
-		print('exec decorator2')
-		return funcs(*args, **kwargs)
-	return wrap
+    print('start decorator2')
+
+    @functools.wraps(funcs)
+    def wrap(*args, **kwargs):
+        print('exec decorator2')
+        return funcs(*args, **kwargs)
+
+    return wrap
+
 
 @decorator2
 @decorator1
 def temp_a():
-	print('hello world')
-
-
+    print('hello world')
 
 
 def multi_process():
-	children = {}
-	def create_subprocess(i):
-		pid = os.fork()
-		if pid == 0:
-			print('子进程:', i, os.getpid(), os.getppid())
-		else:
-			print('父进程', i, os.getpid(), os.getppid())
-		children[pid] = i
-		return pid
-	process_num = os.cpu_count()
-	for i in range(3):
-		pid = create_subprocess(i)
-		if pid == 0:
-			return
-	print(children)
-	while children:
-		pid, statue = os.wait()
-	print(os.getpid(), '多进程结束')
+    children = {}
+
+    def create_subprocess(i):
+        pid = os.fork()
+        if pid == 0:
+            print('子进程:', i, os.getpid(), os.getppid())
+        else:
+            print('父进程', i, os.getpid(), os.getppid())
+        children[pid] = i
+        return pid
+
+    process_num = os.cpu_count()
+    for i in range(3):
+        pid = create_subprocess(i)
+        if pid == 0:
+            return
+    print(children)
+    while children:
+        pid, statue = os.wait()
+    print(os.getpid(), '多进程结束')
+
 
 def target_func(target_name):
-	print(target_name)
+    print(target_name)
+
 
 def multi_process_exec():
-	task_pool = Pool(os.cpu_count())
-	for i in range(10):
-		task_pool.apply_async(target_func, args=('process name: %s' % (i), ))
-	task_pool.close()
-	task_pool.join()
-	print('并发结束')
-
+    task_pool = Pool(os.cpu_count())
+    for i in range(10):
+        task_pool.apply_async(target_func, args=('process name: %s' % (i),))
+    task_pool.close()
+    task_pool.join()
+    print('并发结束')
 
 
 def os_pipe():
-	r, w = os.pipe()
-	pid = os.fork()
-	if pid != 0:
-		reader = os.fdopen(r)
-		os.close(w)
-		print('parent process read!')
-		str = reader.read()
-		print('text: ', str)
-		sys.exit(0)
-	else:
-		os.close(r)
-		writer = os.fdopen(w, 'w')
-		print('child process, write!')
-		writer.write('hello world')
-		writer.close()
-		sys.exit(0)
+    r, w = os.pipe()
+    pid = os.fork()
+    if pid != 0:
+        reader = os.fdopen(r)
+        os.close(w)
+        print('parent process read!')
+        str = reader.read()
+        print('text: ', str)
+        sys.exit(0)
+    else:
+        os.close(r)
+        writer = os.fdopen(w, 'w')
+        print('child process, write!')
+        writer.write('hello world')
+        writer.close()
+        sys.exit(0)
+
 
 def fcntl_fd():
-	fd = open('../note_book', 'r')
-	print(fd)
-	flags = fcntl.fcntl(fd, fcntl.F_GETFD)
-	print(flags)
-	fcntl.fcntl(fd, fcntl.F_SETFD, flags | fcntl.FD_CLOEXEC)
-	print(fd.__class__)
+    fd = open('../note_book', 'r')
+    print(fd)
+    flags = fcntl.fcntl(fd, fcntl.F_GETFD)
+    print(flags)
+    fcntl.fcntl(fd, fcntl.F_SETFD, flags | fcntl.FD_CLOEXEC)
+    print(fd.__class__)
+
 
 def select_epoll():
-	epoll = select.epoll()
-	print(epoll.poll(10))
+    epoll = select.epoll()
+    print(epoll.poll(10))
+
+
+
 if __name__ == '__main__':
-	select_epoll()
+    select_epoll()
