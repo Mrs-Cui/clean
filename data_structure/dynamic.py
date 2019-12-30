677e415d63ac80a7a387d515274d8d0515fa7761
cuixiyi <cuixiyi@meetstarinc.com>
2019-12-30 18:08:32 +0800

    新算法

---
+++ data_structure/dynamic.py
@@ -0,0 +1,144 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import random
+
+#  动态规划练习
+
+
+def max_path():
+    pass
+
+
+"""
+寺庙的柱子是由木头制成。每根柱子必须是一jie完整的木头而且不能是被连接得到的。
+给出n段具有不同长度的木头。你的寺庙有m根高度严格相同的柱子。那么你寺庙最大高度是多少。(m根柱子的高度)
+1<=n<=100000
+1<=m<=100000
+输入：m = 3, [2, 3, 4]
+输出：2 
+输入：m = 3, [2, 10]
+输出：3
+"""
+
+def query_sort(array, start, end):
+    if start > end:
+        return
+    tag = array[start]
+    init_start, init_end = start, end
+    while start < end:
+        while start < end and array[end] >= tag:
+            end -= 1
+        array[start] = array[end]
+        while start < end and array[start] < tag:
+            start += 1
+    array[end] = tag
+    query_sort(array, init_start, start-1)
+    query_sort(array, start + 1, init_end)
+
+def count_max_height(m, trees):
+    length = len(trees)
+    query_sort(trees, 0, length-1)
+    print(trees)
+    if length - m >= 0:
+        start, end = length - m, length - 1
+    else:
+        start, end = 0, length - 1
+    position = list(range(start, end))
+    position.reverse()
+    max_height = 0
+    for cut_num in range(1, m+1):
+        count = 0
+        height = trees[end] // cut_num
+        if height < 1:
+            break
+        count += cut_num
+        for pos in position:
+            if trees[pos] // height >= 1:
+                count += trees[pos] // height
+                if count >= m:
+                    max_height = height if height >= max_height else max_height
+                    break
+    return max_height
+
+
+"""
+在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。
+"""
+
+
+def max_square_size(array):
+    row = len(array)
+    col = len(array[0])
+    sizes = [[0] * col] * row
+    for i in range(col):
+        sizes[0][i] = array[0][i]
+    for i in range(row):
+        sizes[i][0] = array[i][0]
+    max_length = 0
+    for i, items in enumerate(array):
+        if i == 0:
+            continue
+        for j, item in enumerate(items):
+            if j == 0:
+                continue
+            if array[i][j] == 0:
+                sizes[i][j] = 0
+            else:
+                sizes[i][j] = min(sizes[i-1][j-1], sizes[i-1][j], sizes[i][j-1]) + 1
+        max_length = max(sizes[i][j], max_length)
+    print(max_length * max_length)
+
+"""
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+"""
+
+def max_profit(array):
+    length = len(array)
+    buy = -array[0]
+    sell = 0
+    for pos in range(1, length):
+        buy = max(buy, -array[pos])
+        sell = max(sell, array[pos] + buy)
+    print(sell)
+
+"""
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+"""
+
+def max_profit_two(array):
+    length = len(array)
+    if length <= 1:
+        return 0
+    buy = -array[0]
+    sell = 0
+    for pos in range(1, length):
+        sell = max(sell, array[pos] + buy)
+        buy = max(buy, sell - array[pos])
+        print(pos, sell, buy)
+    print(sell)
+
+"""
+给定平面上 n 对不同的点，“回旋镖” 是由点表示的元组 (i, j, k) ，其中 i 和 j 之间的距离和 i 和 k 之间的距离相等（需要考虑元组的顺序）。
+找到所有回旋镖的数量。你可以假设 n 最大为 500，所有点的坐标在闭区间 [-10000, 10000] 中。
+"""
+
+
+
+def boomerang(array):
+    pass
+
+
+if __name__ == '__main__':
+    # trees = [random.randint(1, 10) for i in range(3)]
+    # print(count_max_height(4, trees))
+    array = [
+        [1, 0, 1, 0, 0],
+        [1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 1, 0],
+    ]
+    max_profit_two([7,1,2,10,4,6])
---
+++ data_structure/figure.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+"""
+    最小生成树
+"""
+
+"""
+    普利姆算法:
+        首先先任意选出一个节点作为开始节点. 找出开始节点到其他节点最小距离,
+        将该点记录下来. 设置两个集合 U, V.U集合存放找到最短距离的节点,
+        V集合存放未找到最短距离的节点. 
+"""
+
+def mini_span_tree(weight_map):
+    nodes = [key for key in weight_map.keys()]
+    weights = dict()
+    finish_nodes = []
+    finish_nodes.append(nodes.pop(0))
+    while nodes:
+        min_weight = float('inf')
+        for finish_node in finish_nodes:
+            for node in nodes:
+                if weight_map[finish_node].get(node, float('inf')) < min_weight:
+                    min_node = node
+                    min_weight = weight_map[finish_node][node]
+        else:
+            finish_nodes.append(min_node)
+            weights[min_node] = min_weight
+            nodes.remove(min_node)
+        print(weights, finish_nodes)
+
+"""
+    Kruskal算法:
+"""
+"""
+    对所有权边排序, 每次选取最小边, 直到完成最小生成树. n-1条边中不能有回路.
+"""
+
+def mini_span_tree_2(weight_map):
+    weight = []
+    for key, value in weight_map.items():
+        for node, weight_value in value.items():
+            weight.append((key, node, weight_value))
+    weight = sorted(weight, key=lambda item: item[2], reverse=False)
+
+
+if __name__ == '__main__':
+    weight_map = {
+        'A': {
+            'B': 6,
+            'C': 3
+        },
+        'B': {
+            'A': 6,
+            'C': 4,
+            'D': 2,
+            'F': 3,
+        },
+        'C': {
+            'A': 3,
+            'B': 4,
+            'F': 7,
+            'E': 8
+        },
+        'D': {
+            'B': 2,
+            'F': 6
+        },
+        'E': {
+            'C': 8,
+            'F': 7
+        },
+        'F': {
+            'B': 3,
+            'C': 7,
+            'D': 6,
+            'E': 7
+        }
+    }
+    mini_span_tree(weight_map)
