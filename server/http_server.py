12a71853cffeed7618c83d5071b6115ac451d4a0
cuixiyi <cuixiyi@meetstarinc.com>
2020-09-11 16:02:11 +0800

    tornado http server

--- server/http_server.py
+++ server/http_server.py
@@ -1,13 +1,15 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 import os
-
+import requests
+import time
 from tornado.httpserver import HTTPServer, HTTPRequest
 from tornado.ioloop import IOLoop
 from tornado import netutil
 from tornado import httputil
 from tornado import concurrent
 from tornado import web, gen, process, httpclient
+from concurrent.futures import ThreadPoolExecutor
 
 
 class NewHttpServer(HTTPServer):
@@ -39,9 +41,53 @@ class HandleRequest(web.RequestHandler):
     def request_callback(self, response):
         self.finish()
 
+
+class NewHandleRequest(web.RequestHandler):
+    executor = ThreadPoolExecutor(os.cpu_count() * 2)
+
+    @gen.coroutine
+    def get(self, *args, **kwargs):
+        urls = ['https://www.baidu.com',] * 5
+        print(urls)
+        results = []
+        start = time.time()
+        for url in urls:
+            request = httpclient.HTTPRequest(url=url)
+            future = self.handle(request)
+            results.append(future)
+        for future in results:
+            print(future.result())
+        self.finish('hello world')
+        print('spend time: ', time.time() - start)
+
+    @gen.coroutine
+    @web.asynchronous
+    def post(self, *args, **kwargs):
+        urls = ['https://www.baidu.com',] * 5
+        results = []
+        start = time.time()
+        print(urls)
+        client = httpclient.AsyncHTTPClient()
+
+        for url in urls:
+            request = httpclient.HTTPRequest(url=url)
+            result = client.fetch(request, callback=self.finish_response)
+            print('result', result)
+            results.append(result)
+        print('response results:', results)
+        print('post spend time:', time.time() - start)
+
+    def finish_response(self, response):
+        self.finish('hello world')
+
+    @concurrent.run_on_executor()
+    def handle(self, request):
+        response = requests.get(request.url)
+        return response.content
+
 if __name__ == '__main__':
     app = web.Application([
-        (r'/handle_request', HandleRequest)
+        (r'/handle_request', NewHandleRequest)
     ])
     sockets = netutil.bind_sockets(6666, reuse_port=True)
     process.fork_processes(os.cpu_count() * 2)
